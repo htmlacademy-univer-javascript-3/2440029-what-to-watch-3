bc05d70470f90d5b19e1488e85ab773e9d91795c
StepaZet <stepa.zet2@gmail.com>
2023-11-13 00:08:31 +0500

    task 5.1 done

---
+++ src/utils/video-player.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable no-unused-expressions */
+
+import React, { useRef, useEffect } from 'react';
+
+type Props = {
+  videoSource: string;
+  poster: string;
+  autoplay: boolean;
+};
+
+const VideoPlayer: React.FC<Props> = ({ videoSource, poster, autoplay }) => {
+  const videoElementRef = useRef<HTMLVideoElement>(null);
+
+  const playVideo = () => {
+    if (videoElementRef.current) {
+      videoElementRef.current.play();
+    }
+  };
+
+  const pauseAndReloadVideo = () => {
+    const videoElement = videoElementRef.current;
+    if (videoElement) {
+      videoElement.pause();
+      videoElement.load();
+    }
+  };
+
+  useEffect(() => {
+    autoplay ? playVideo() : pauseAndReloadVideo();
+  }, [autoplay]);
+
+  return (
+    <video
+      ref={videoElementRef}
+      width="280"
+      height="175"
+      src={videoSource}
+      poster={poster}
+      muted
+      loop
+    />
+  );
+};
+
+export default VideoPlayer;
